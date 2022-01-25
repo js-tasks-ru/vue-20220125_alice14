aa0e307b083c5d38a752761b1bef1d02b926c023
Alice <alice.tolokova@ya.ru>
2022-01-25 22:18:42 +0300

    Vue Calculator

--- 01-basics/02-VueCalculator/script.js
+++ 01-basics/02-VueCalculator/script.js
@@ -1,3 +1,32 @@
-// import { createApp } from './vendor/vue.esm-browser.js';
+import { createApp } from './vendor/vue.esm-browser.js';
 
-// Создайте Vue приложение
+const vm = createApp({
+  data() {
+    return {
+      result: 0,
+      firstValue: 0,
+      secondValue: 0,
+      calcType: 'sum',
+    };
+  },
+  methods: {
+    calculate() {
+      switch (this.calcType) {
+        case 'sum':
+          this.result = this.firstValue + this.secondValue;
+          break;
+        case 'subtract':
+          this.result = this.firstValue - this.secondValue;
+          break;
+        case 'multiply':
+          this.result = this.firstValue * this.secondValue;
+          break;
+        case 'divide':
+          this.result = this.firstValue / this.secondValue;
+          break;
+        default:
+          break;
+      }
+    },
+  },
+}).mount('#app');
